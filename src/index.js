424ed1b4e71e456970d01c716ead0694364713b3
Daniela Gavidia <daniela@miro.com>
2023-11-01 11:04:07 +0100

    Subscribe to selection:update from main iframe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 async function init() {
+  await miro.board.ui.on('selection:update', e => {
+    console.log("MAIN OUTPUT: ", e)
+  })
+
   miro.board.ui.on('icon:click', async () => {
     await miro.board.ui.openPanel({url: 'app.html'});
   });
